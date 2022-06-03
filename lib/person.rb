856509c243f4227baf33a5c2b6295b3b7ea4bdce
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-03 13:16:25 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   #your code here
+
+  # copied directly from course notes
+  def initialize(attributes)
+    attributes.each do |key, value| 
+        self.class.attr_accessor(key)
+        self.send(("#{key}="), value)
+    end
+  end
 end
